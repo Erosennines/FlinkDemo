bbb8adfacf7489d408364dbc81f9c5302cdccc10
Erosennines <1143765981@qq.com>
2021-01-26 11:10:05 +0800

    02_从文件中读取数据

--- src/main/java/chapter1/Flink02_Source_File.java
+++ src/main/java/chapter1/Flink02_Source_File.java
@@ -1,2 +1,16 @@
-package chapter1;public class Flink02_Source_File {
+package chapter1;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class Flink02_Source_File {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 从文件中读取数据
+        DataStreamSource<String> stream = env.readTextFile("in/C1_02.txt");
+        stream.print();
+
+        env.execute("Flink02_Source_File");
+    }
 }
