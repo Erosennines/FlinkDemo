ab18a6bfc4c26192d6e707e62a12cee0d49dd0fb
Erosennines <1143765981@qq.com>
2021-02-01 14:06:37 +0800

    redis sink

---
+++ src/main/java/chapter3/Sink02_RedisSink.java
@@ -0,0 +1,63 @@
+package chapter3;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+import pojo.WaterSensor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sink02_RedisSink {
+    // kafka sink
+    public static void main(String[] args) throws Exception {
+        // 首先需要添加redis Connector依赖
+        List<WaterSensor> waterSensors = new ArrayList<>();
+
+        waterSensors.add(new WaterSensor("sensor_1", 1607527992000L, 20));
+        waterSensors.add(new WaterSensor("sensor_1", 1607527994000L, 50));
+        waterSensors.add(new WaterSensor("sensor_1", 1607527996000L, 50));
+        waterSensors.add(new WaterSensor("sensor_2", 1607527993000L, 10));
+        waterSensors.add(new WaterSensor("sensor_2", 1607527995000L, 30));
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<WaterSensor> stream = env.fromCollection(waterSensors);
+
+        // 将流中的内容写入redis
+
+        // 首先创建redis的连接器
+        FlinkJedisPoolConfig redisConfig = new FlinkJedisPoolConfig.Builder()
+                .setHost("hadoop122")
+                .setPort(6379)
+                .setMaxTotal(100)
+                .setTimeout(1000 * 10)
+                .build();
+
+        // 指定kafka主机+端口，主题名，序列化方式
+        stream.addSink(new RedisSink<>(redisConfig, new RedisMapper<WaterSensor>() {
+            @Override
+            public RedisCommandDescription getCommandDescription() {
+                return new RedisCommandDescription(RedisCommand.HSET, "sensor");
+            }
+
+            @Override
+            public String getKeyFromData(WaterSensor waterSensors) {
+                return waterSensors.getId();
+            }
+
+            @Override
+            public String getValueFromData(WaterSensor waterSensors) {
+                return JSON.toJSONString(waterSensors);
+            }
+        }));
+
+        env.execute();
+    }
+}
