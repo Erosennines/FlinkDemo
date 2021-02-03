db876d0501124e71c412481f9668ca83f0a55c1b
Erosennines <1143765981@qq.com>
2021-02-03 10:44:30 +0800

    Project05 POJO

---
+++ src/main/java/practice1/pojo/TxEvent.java
@@ -0,0 +1,14 @@
+package practice1.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TxEvent {
+    private String txId;
+    private String payChannel;
+    private Long eventTime;
+}
